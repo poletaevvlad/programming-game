a35c298c2a4637bbfa39a42a5c8d5bbf151c7e95
Vlad Poletaev <poletaev.vladislav@gmail.com>
2017-09-15 09:08:40 +0300

    Реализовал скрипт перемещения камеры для центрирования точки (0, 0, 0) с учетом произвольных полей

---
+++ Assets/Scripts/BoardResizer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class BoardResizer : MonoBehaviour {
+
+	public Camera mainCamera;
+		
+	[Header("Paddings")]
+	public float leftPadding = 0;
+	public float topPadding = 0;
+	public float rightPadding = 0;
+	public float bottomPadding = 0;
+
+	void Start () {
+		if (mainCamera == null) {
+			Debug.LogError ("Main camera property must be set for BoardResizer script");
+		}
+	}
+		
+	public void Resize () {
+		Vector3 topLeftViewportPoint = mainCamera.ViewportToWorldPoint (new Vector3 (0, 0));
+		Vector3 bottomRightViewportPoint = mainCamera.ViewportToWorldPoint (new Vector3 (1, 1));
+		float viewportWidth = bottomRightViewportPoint.x - topLeftViewportPoint.x;
+		float viewportHeight = bottomRightViewportPoint.y - topLeftViewportPoint.y;
+
+		Vector2 onScreenOrigin = new Vector2 ((Screen.width + leftPadding - rightPadding) / 2 / Screen.width, 
+			(Screen.height + topPadding - bottomPadding) / 2 / Screen.height);
+
+		Vector3 cameraPosition = new Vector3 (-viewportWidth * (onScreenOrigin.x - 0.5f),
+			viewportHeight * (onScreenOrigin.y - 0.5f), mainCamera.transform.position.z);
+		mainCamera.transform.position = cameraPosition;
+	}
+}
