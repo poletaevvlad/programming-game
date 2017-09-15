ce309f7daf513e43681bb9b20a3284e11c725b9c
Vlad Poletaev <poletaev.vladislav@gmail.com>
2017-09-15 18:53:45 +0300

    Реализовал выравнивание сетки уровня с границами пикселя экрана

--- Assets/Scripts/BoardResizer.cs
+++ Assets/Scripts/BoardResizer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
+[ExecuteInEditMode]
 public class BoardResizer : MonoBehaviour {
 
 	public Camera mainCamera;
@@ -13,37 +14,55 @@ public class BoardResizer : MonoBehaviour {
 	public float rightPadding = 0;
 	public float bottomPadding = 0;
 
+	[Header("Size")]
+	// Temporary variables. Will be removed when model is implemented
+	public int width;
+	public int height;
+
+	/* Storing screen dimensions in order to detect its change */
+	private int screenWidth = 0;
+	private int screenHeight = 0;
+
 	void Start () {
 		if (mainCamera == null) {
 			Debug.LogError ("Main camera property must be set for BoardResizer script");
 		}
 	}
+
+	void Update(){
+		if (Screen.width != screenWidth || Screen.height != screenHeight) {
+			screenWidth = Screen.width;
+			screenHeight = Screen.height;
+			Resize ();
+		}
+	}
+
+	void OnValidate(){
+		// Reseting screen width. Resize() will be called on next Update.
+		screenWidth = 0;
+	}
 		
-	private float scaleToHeight(float targetHeight, float padding){
-		return Screen.height * (targetHeight / 2f) / (Screen.height - padding);
-	}
-
-	private float scaleToWidth(float targetWidth, float padding, float aspectRatio){
-		return Screen.width * (targetWidth / 2f) / (Screen.width - padding) / aspectRatio;
-	}
-
 	public void Resize () {
-		float targetWidth = transform.localScale.x;
-		float targetHeight = transform.localScale.y;
+		transform.localScale = new Vector3(width + 1f / 32f, height + 1f / 32f, 1);
+		GetComponent<MeshRenderer> ().sharedMaterial.SetTextureScale ("_MainTex", 
+			new Vector2 (width + 1f / 32f, height + 1f/32f));
 
-		float orthographicSize = Mathf.Max (scaleToHeight (targetHeight, topPadding + bottomPadding), 
-			                         scaleToWidth (targetWidth, leftPadding + rightPadding, mainCamera.aspect));
-		mainCamera.orthographicSize = orthographicSize;
+		//TODO: Add additional grid texture sizes
+		float scale = screenHeight / 32f / 2f;
+		mainCamera.orthographicSize = scale;
+		mainCamera.transform.localPosition = new Vector3 (-1f / 64, -1f / 64, -10);
 
 		Vector3 topLeftViewportPoint = mainCamera.ViewportToWorldPoint (new Vector3 (0, 0));
 		Vector3 bottomRightViewportPoint = mainCamera.ViewportToWorldPoint (new Vector3 (1, 1));
 		float viewportWidth = bottomRightViewportPoint.x - topLeftViewportPoint.x;
 		float viewportHeight = bottomRightViewportPoint.y - topLeftViewportPoint.y;
 
-		Vector2 onScreenOrigin = new Vector2 ((Screen.width + leftPadding - rightPadding) / 2 / Screen.width, 
-			(Screen.height + topPadding - bottomPadding) / 2 / Screen.height);
+		Vector2 onScreenOrigin = new Vector2 ((screenWidth + leftPadding - rightPadding) / 2 / screenWidth, 
+			(screenHeight + topPadding - bottomPadding) / 2 / screenHeight);
 		Vector3 cameraPosition = new Vector3 (-viewportWidth * (onScreenOrigin.x - 0.5f),
 			viewportHeight * (onScreenOrigin.y - 0.5f), mainCamera.transform.position.z);
+		cameraPosition.x = (int)(cameraPosition.x * 32f) / 32f + (1 - screenWidth %	 2) / 64f;
+		cameraPosition.y = (int)(cameraPosition.y * 32f) / 32f + (1 - screenHeight % 2) / 64f;
 		mainCamera.transform.position = cameraPosition;
 	}
 }
