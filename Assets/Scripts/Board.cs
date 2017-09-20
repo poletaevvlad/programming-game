004db16c4e2f595be8ce2cb88d3117a92b7948d1
Vlad Poletaev <poletaev.vladislav@gmail.com>
2017-09-20 16:42:42 +0300

    Добавил аттрибуты сериализации для полей класса Board

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -1,12 +1,19 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[Serializable]
 public class Board {
 
 	Component[] component { get; set;}
-	public int width { get; set;}
-	public int heigth { get; set;}
+
+	[SerializeField]
+	public int width;
+
+	[SerializeField]
+	public int heigth;
+
 	ConnectionLine connections { get; set;}
 
 
