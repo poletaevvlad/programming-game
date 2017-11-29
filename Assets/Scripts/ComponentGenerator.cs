f7af585722a06ba909b8f9d0d05732a07d019d83
Vlad Poletaev <poletaev.vladislav@gmail.com>
2017-11-30 00:44:39 +0300

    Добавление индикаторов интерфейса компонента

---
+++ Assets/Scripts/ComponentGenerator.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComponentGenerator : MonoBehaviour {
+
+    public int column;
+    public int row;
+    public int width;
+    public int height;
+    public IOBuilderParameters parameters;
+
+    private BoardResizer boardResizer = null;
+
+    private void RequestResizer(){
+        if (boardResizer == null) {
+            boardResizer = GameObject.Find("/Board").GetComponent<BoardResizer>();
+        }
+    }
+
+    [ContextMenu("Update all")]
+    public void UpdateAll() {
+        Position();
+        Clear();
+        Generate();
+    }
+
+    [ContextMenu("Update transform")]
+    public void Position() {
+        RequestResizer();
+        float xPosition = boardResizer.GetLeft(column) + boardResizer.CellWidth * width / 2;
+        float yPosition = boardResizer.GetTop(row) - boardResizer.CellHeight * height / 2;
+        transform.position = new Vector3(xPosition, yPosition, transform.position.z);
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.size = new Vector2(width * boardResizer.CellWidth, height * boardResizer.CellHeight);
+    }
+    
+    private void InstantiateCircle(int r, int c, Vector3 relativePosition){
+        Transform newObject = Instantiate(parameters.circlePrefab, transform);
+        newObject.localPosition = relativePosition + new Vector3(c - (width - 1) / 2f, -r + (height - 1) / 2f);
+    }
+    
+    [ContextMenu("Clear content")]
+    public void Clear(){
+        while (transform.childCount > 0) {
+            GameObject.DestroyImmediate(transform.GetChild(0).gameObject);
+        }
+    }
+
+    [ContextMenu("Generate content")]
+    public void Generate() {
+        for (int x = 0; x < width; x++) {
+            InstantiateCircle(0, x, parameters.topCirclePosition);
+            InstantiateCircle(height - 1, x, parameters.bottomCirclePosition);
+        }
+        for (int y = 0; y < height; y++) {
+            InstantiateCircle(y, 0, parameters.leftCirclePosition);
+            InstantiateCircle(y, width - 1, parameters.rightCirclePosition);
+        }
+    }
+
+}
---
+++ Assets/Scripts/IOBuilderParameters.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "IOParameters", menuName = "IO Builder parameters")]
+public class IOBuilderParameters : ScriptableObject {
+    [Header("Circle relative position")]
+    public Vector3 leftCirclePosition;
+    public Vector3 topCirclePosition;
+    public Vector3 rightCirclePosition;
+    public Vector3 bottomCirclePosition;
+
+    [Header("Left arrow")]
+    public Vector3 leftArrowPosition;
+
+    [Range(0, 360)]
+    public float leftArrowRotation;
+
+    [Header("Top arrow")]
+    public Vector3 topArrowPosition;
+
+    [Range(0, 360)]
+    public float topArrowRotation;
+
+    [Header("Right arrow")]
+    public Vector3 rightArrowPosition;
+
+    [Range(0, 360)]
+    public float rightArrowRotation;
+
+    [Header("Bottom arrow")]
+    public Vector3 bottomArrowPosition;
+
+    [Range(0, 360)]
+    public float bottomArrowRotation;
+
+    [Header("Prefabs")]
+    public Transform arrowPrefab;
+    public Transform circlePrefab;
+
+}
