8c24157c11e128ee6342bf7a2b02b873ba0e853f
Vlad Poletaev <poletaev.vladislav@gmail.com>
2017-12-03 19:32:04 +0300

    Реализован выбор положения начала рисвания линии

--- Assets/Scripts/ConnectionLineRenderer.cs
+++ Assets/Scripts/ConnectionLineRenderer.cs
@@ -9,7 +9,8 @@ public class ConnectionLineRenderer : MonoBehaviour {
     private int length;
 
     public float width;
-    public float visibleDist;
+    public float skipStartDist;
+    public float skipEndDist;
 
     private BoardResizer _boardResizer = null;
     public BoardResizer boardResizer {
@@ -26,48 +27,62 @@ public class ConnectionLineRenderer : MonoBehaviour {
                            boardResizer.GetTop(coord.y) - boardResizer.CellHeight / 2);
     }
 
-    private List<Vector3> vertices;
-    private int[] triangles;
-
-    public void UpdateLine() {
-        vertices = new List<Vector3>();
-
+    public void UpdateLine()
+    {
         float distSqrt2 = width * Mathf.Sqrt(2);
 
-        float angle = Mathf.Atan2(coordinates[0].y - coordinates[1].y, -coordinates[0].x + coordinates[1].x);
-        Vector3 point = CoordToVec3(coordinates[0]);
-        vertices.Add(point + distSqrt2 * new Vector3(Mathf.Cos(angle - 3 * Mathf.PI / 4), Mathf.Sin(angle - 3 * Mathf.PI / 4)));
-        vertices.Add(point + distSqrt2 * new Vector3(Mathf.Cos(angle + 3 * Mathf.PI / 4), Mathf.Sin(angle + 3 * Mathf.PI / 4)));
-        for (int i = 1; i < coordinates.Length - 1; i++) {
-            float bAngle = Mathf.Atan2(coordinates[i].y - coordinates[i + 1].y, -coordinates[i].x + coordinates[i + 1].x);
-            point = CoordToVec3(coordinates[i]);
-            vertices.Add(point + width / 2f / Mathf.Pow(Mathf.Cos((bAngle - angle) / 2), 2) *
-                                          new Vector3(Mathf.Cos(angle - Mathf.PI / 2) + Mathf.Cos(bAngle - Mathf.PI / 2),
-                                                      Mathf.Sin(angle - Mathf.PI / 2) + Mathf.Sin(bAngle - Mathf.PI / 2)));
-            vertices.Add(point + width / 2f / Mathf.Pow(Mathf.Cos((bAngle - angle) / 2), 2) *
-                                          new Vector3(Mathf.Cos(angle + Mathf.PI / 2) + Mathf.Cos(bAngle + Mathf.PI / 2),
-                                                      Mathf.Sin(angle + Mathf.PI / 2) + Mathf.Sin(bAngle + Mathf.PI / 2)));
-            angle = bAngle;
+        List<Vector3> vertices = new List<Vector3>();
+        int i = 0;
+        float skipLength = Mathf.Max(skipStartDist, 0);
+        while (i < coordinates.Length - 1) {
+            float segmentDistance = ManhattanDistance(coordinates[i], coordinates[i + 1]);
+            if (skipLength < segmentDistance) {
+                break;
+            } else {
+                skipLength -= segmentDistance;
+                i++;
+            }
         }
-        point = CoordToVec3(coordinates[coordinates.Length - 1]);
-        vertices.Add(point + distSqrt2 * new Vector3(Mathf.Cos(angle + 7 * Mathf.PI / 4), Mathf.Sin(angle + 7 * Mathf.PI / 4)));
-        vertices.Add(point + distSqrt2 * new Vector3(Mathf.Cos(angle - 7 * Mathf.PI / 4), Mathf.Sin(angle - 7 * Mathf.PI / 4)));
+        if (i < coordinates.Length - 1) {
+            Debug.Log(skipLength);
+            float angle = Mathf.Atan2(coordinates[i].y - coordinates[i + 1].y, -coordinates[i].x + coordinates[i + 1].x);
+            Vector3 point = Vector3.Lerp(CoordToVec3(coordinates[i]), CoordToVec3(coordinates[i + 1]), skipLength);
+            vertices.Add(point + distSqrt2 * new Vector3(Mathf.Cos(angle - 3 * Mathf.PI / 4), Mathf.Sin(angle - 3 * Mathf.PI / 4)));
+            vertices.Add(point + distSqrt2 * new Vector3(Mathf.Cos(angle + 3 * Mathf.PI / 4), Mathf.Sin(angle + 3 * Mathf.PI / 4)));
+            for (i++; i < coordinates.Length - 1; i++) {
+                float bAngle = Mathf.Atan2(coordinates[i].y - coordinates[i + 1].y, -coordinates[i].x + coordinates[i + 1].x);
+                point = CoordToVec3(coordinates[i]);
+                vertices.Add(point + width / 2f / Mathf.Pow(Mathf.Cos((bAngle - angle) / 2), 2) *
+                                              new Vector3(Mathf.Cos(angle - Mathf.PI / 2) + Mathf.Cos(bAngle - Mathf.PI / 2),
+                                                          Mathf.Sin(angle - Mathf.PI / 2) + Mathf.Sin(bAngle - Mathf.PI / 2)));
+                vertices.Add(point + width / 2f / Mathf.Pow(Mathf.Cos((bAngle - angle) / 2), 2) *
+                                              new Vector3(Mathf.Cos(angle + Mathf.PI / 2) + Mathf.Cos(bAngle + Mathf.PI / 2),
+                                                          Mathf.Sin(angle + Mathf.PI / 2) + Mathf.Sin(bAngle + Mathf.PI / 2)));
+                angle = bAngle;
+            }
+            point = CoordToVec3(coordinates[coordinates.Length - 1]);
+            vertices.Add(point + distSqrt2 * new Vector3(Mathf.Cos(angle + 7 * Mathf.PI / 4), Mathf.Sin(angle + 7 * Mathf.PI / 4)));
+            vertices.Add(point + distSqrt2 * new Vector3(Mathf.Cos(angle - 7 * Mathf.PI / 4), Mathf.Sin(angle - 7 * Mathf.PI / 4)));
+            int[] triangles = new int[3 * vertices.Count - 6];
 
-        triangles = new int[3 * vertices.Count - 6];
-        for (int i = 0; i < vertices.Count - 2; i += 2) {
-            triangles[3 * i + 0] = i;
-            triangles[3 * i + 1] = triangles[3 * i + 4] = i + 1;
-            triangles[3 * i + 2] = triangles[3 * i + 3] = i + 2;
-            triangles[3 * i + 5] = i + 3;
+            for (int j = 0; j < vertices.Count - 2; j += 2) {
+                triangles[3 * j + 0] = j;
+                triangles[3 * j + 1] = triangles[3 * j + 4] = j + 1;
+                triangles[3 * j + 2] = triangles[3 * j + 3] = j + 2;
+                triangles[3 * j + 5] = j + 3;
+            }
+
+            Mesh mesh = new Mesh();
+            mesh.SetVertices(vertices);
+            mesh.SetTriangles(triangles, 0);
+            GetComponent<MeshFilter>().mesh = mesh;
+        } else {
+            GetComponent<MeshFilter>().mesh = null;
         }
-
-        Mesh mesh = new Mesh();
-        mesh.SetVertices(vertices);
-        mesh.SetTriangles(triangles, 0);
-        GetComponent<MeshFilter>().mesh = mesh;
     }
 
-    public void Update(){
+    public void Update() {
+        UpdateLine();
     }
 
     private static int ManhattanDistance(Coord a, Coord b){
@@ -78,16 +93,6 @@ public class ConnectionLineRenderer : MonoBehaviour {
         Gizmos.color = Color.black; 
         for (int i = 1; i < coordinates.Length; i++) {
             Gizmos.DrawLine(CoordToVec3(coordinates[i - 1]), CoordToVec3(coordinates[i]));
-        }
-
-        Gizmos.color = Color.blue;
-        foreach (Vector3 vec in vertices) {
-            Gizmos.DrawSphere(vec, 0.05f);
-        }
-        for (int i = 0; i < triangles.Length; i+= 3) {
-            Gizmos.DrawLine(vertices[triangles[i]], vertices[triangles[i + 1]]);
-            Gizmos.DrawLine(vertices[triangles[i]], vertices[triangles[i + 2]]);
-            Gizmos.DrawLine(vertices[triangles[i + 1]], vertices[triangles[i + 2]]);
         }
     }
 
