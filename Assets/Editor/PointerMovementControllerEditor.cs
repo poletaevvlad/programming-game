190185ca289e9a300f5a96046a99ce59ec04a782
Vlad Poletaev <poletaev.vladislav@gmail.com>
2017-12-05 19:05:43 +0300

    Определение координат клетки под курсором мыши

---
+++ Assets/Editor/PointerMovementControllerEditor.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+//[CustomEditor(typeof(PointerMovementController))]
+class PointerMovementControllerEditor : Editor {
+
+    public override void OnInspectorGUI() {
+        PointerMovementController controller = (PointerMovementController)target;
+        controller.raycastCamera = (Camera)EditorGUILayout.ObjectField("Raycast Camera", controller.raycastCamera, typeof(Camera), true);
+
+        EditorGUILayout.LabelField("Column (y):", controller.CurrentY.ToString());
+        EditorGUILayout.LabelField("Row (x):", controller.CurrentX.ToString()); 
+    }
+
+}
--- Assets/Scripts/PointerMovementController.cs
+++ Assets/Scripts/PointerMovementController.cs
@@ -1,15 +1,25 @@
 ﻿using UnityEngine;
-using UnityEngine.UI;
 
+[RequireComponent(typeof(BoardModel))]
 public class PointerMovementController : MonoBehaviour {
 
     public Camera raycastCamera = null;
 
     private Transform previousHover = null;
 
-	void Update () {
+    public int CurrentX;
+    public int CurrentY;
+
+    private BoardModel model;
+
+    private void Start() {
+        model = GetComponent<BoardModel>();
+    }
+
+    void Update () {
         Ray ray = raycastCamera.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
+        UpdatePoint(ray.origin);
 
         Transform newHover = null;
         if (Physics.Raycast(ray, out hit)) {
@@ -26,5 +36,13 @@ public class PointerMovementController : MonoBehaviour {
             }
             previousHover = newHover;
         }
+    }
+
+    private void UpdatePoint(Vector3 position) {
+        CurrentX = (int)(position.x + model.board.width / 2f);
+        CurrentY = (int)(model.board.heigth - position.y - model.board.heigth / 2f);
+        if (CurrentX < 0 || CurrentX >= model.board.width || CurrentY < 0 || CurrentY >= model.board.heigth) {
+            CurrentX = CurrentY = -1;
+        } 
     }
 }
