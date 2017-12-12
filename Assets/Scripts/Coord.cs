10dc2934e3a8fe715d9d1d38bc2cdae8865252e1
Alexander Gurin <alexandergurin1996@gmail.com>
2017-12-12 23:35:44 +0300

    Сделал конструктор

--- Assets/Scripts/Coord.cs
+++ Assets/Scripts/Coord.cs
@@ -5,7 +5,14 @@ using UnityEngine;
 
 [System.Serializable]
 public struct Coord{
-
     public int x;
     public int y;
+
+    public Coord(int x, int y) {
+        this.x = x;
+        this.y = y; 
+    }
+
+    //Сделать конструктор без парамтеров нельзя в структуре
+    //public Coord(): this(0, 0) { }
 }
