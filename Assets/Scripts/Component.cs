af399e694f636ba995be4a82976e2d5f3d1259d6
Alexander Gurin <alexandergurin1996@gmail.com>
2017-12-15 21:58:53 +0300

    Добавил тип нового компонента("memory") и реализовал функцию получения компонента по типу

---
+++ Assets/Scripts/Component.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+	
+public enum ComponentTypeIndex{
+	Addition,
+	Conditional,
+	LineSplit,
+	LineIntersection,
+    Memory
+}
+
+public class Component {
+    int id { get; set;}
+    public Coord coord { get; set; }
+	public ComponentTypeIndex type { get; set;}
+}
