a1e194583c3d9fd9df67e9428f4a2643633f6ede
Vlad Poletaev <poletaev.vladislav@gmail.com>
2017-12-16 19:43:45 +0300

    Автоматическое изменение размера прокручиваемой области для реализации неограниченной в одном направлении прокрутки

---
+++ Assets/Scripts/UI/ScrollAutoExtension.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(ScrollRect))]
+public class ScrollAutoExtension : MonoBehaviour {
+
+    private ScrollRect scrollRect;
+    private RectTransform rectTransform;
+    public RectTransform viewportTransform;
+    public float addedSize;
+
+	void Start () {
+        scrollRect = GetComponent<ScrollRect>();
+        rectTransform = GetComponent<RectTransform>();
+
+        OnScrolled();
+	}
+
+    public void OnScrolled(){
+        viewportTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, 
+                rectTransform.rect.width - viewportTransform.localPosition.x + addedSize);
+    }
+	
+	
+}
