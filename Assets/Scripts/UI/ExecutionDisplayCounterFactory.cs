1a0d1989cae14fdf69b0309174d9e89e38453e33
Vlad Poletaev <poletaev.vladislav@gmail.com>
2017-12-16 19:13:23 +0300

    Добавлен шрифт для UI

--- Assets/Scripts/UI/ExecutionDisplayCounterFactory.cs
+++ Assets/Scripts/UI/ExecutionDisplayCounterFactory.cs
@@ -5,12 +5,12 @@ using UnityEngine.UI;
 public class ExecutionDisplayCounterFactory : ExecutionDisplayElementFactory{
 
     public Text textPrefab;
+    public int initialValue;
 
     public override RectTransform CreateElement(int index, Transform parent){
         Text text = Instantiate(textPrefab, parent);
-        text.text = index.ToString();
+        text.text = (index + initialValue).ToString();
         return text.GetComponent<RectTransform>();
-
     }
 
 }
